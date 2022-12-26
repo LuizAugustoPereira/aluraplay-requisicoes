22ffe2fd0115cc025f483b4fafe6eb658c38d47b
unknown <gutinholadeira@hotmail.com>
2022-12-26 14:45:23 -0300

    last commit

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -19,12 +19,23 @@ async function criaVideo(titulo, descricao, url, imagem) {
             imagem: imagem
         })
     })
+    if(!conexao.ok) {
+        throw new Error('Não foi possível enviar o vídeo')
+    }
 
     const conexaoConvertida = await conexao.json()
     return conexaoConvertida
 }
 
+async function buscaVideos(termoDeBusca) {
+    const conexao = await fetch(`http://localhost:3000/videos?q=${termoDeBusca}`)
+    const conexaoConvertida = await conexao.json()
+
+    return conexaoConvertida
+}
+
 export const conectaApi = {
     listaVideos,
-    criaVideo
+    criaVideo,
+    buscaVideos
 }
